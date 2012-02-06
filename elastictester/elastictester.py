ddf63b868696b67d2bc2a10bb25c038751948d8e
Seth Blank <s@yourtrove.com>
2012-02-06 14:57:10 -0800

    skeleton

---
+++ elastictester/elastictester.py
@@ -0,0 +1,19 @@
+import pyes
+
+# stop elasticsearch if running
+# nuke its database
+# start it up
+
+# for each test:
+  # set index parameters
+  # index all documents in payload/
+  # query a gajillion ways
+    # save results
+  # delete db
+
+# compare results with expectations
+
+
+# needed:
+  # a way to specify index variance
+  # a way to specify query variance

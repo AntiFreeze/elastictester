5b0ceecf7dc63c3afcb3f12157a0846df7c52659
Seth Blank <s@yourtrove.com>
2012-02-06 14:44:43 -0800

    settings documentation

--- elastictester/settings.py
+++ elastictester/settings.py
@@ -1,8 +1,13 @@
 
-# ELASTICSEARCH_BASE and ELASTICSEARCH_PORT
+# ELASTICSEARCH_BASE and ELASTICSEARCH_URL
 # must be defined in local_settings
+#
 # BASE is the root under which /bin, /data
-# and /config live
+# and /config live if you want the tester
+# to touch this data
+#
+# URL is where to POST/GET data do
+
 try:
     from local_settings import *    
 except ImportError:
